90fe5c4d08494f69a21c3dd96037eb543ee9b315
MarcoMeinardi <marcomeinardi01@gmail.com>
2023-03-30 15:56:50 +0200

    Add test for `CardsDeck`

--- src/test/java/model/CardsDeckTest.java
+++ src/test/java/model/CardsDeckTest.java
@@ -1,7 +1,12 @@
 package model;
 
 import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+
 import org.junit.Test;
+
+import java.util.Optional;
 
 public class CardsDeckTest {
 
@@ -17,5 +22,17 @@ public class CardsDeckTest {
     public void testSize() {
         CardsDeck deck = new CardsDeck();
         assertEquals(132, deck.size());
+    }
+
+    @Test
+    public void TestFullDraw() {
+        CardsDeck deck = new CardsDeck();
+        for (int i = 0; i < 132; i++) {
+            Optional<Card> card = deck.draw();
+            assertFalse(card.isEmpty());
+            assertNotEquals(Card.Empty, card.get());
+        }
+        assertEquals(0, deck.size());
+        assertEquals(Optional.empty(), deck.draw());
     }
 }
