6847b1d8bb7617bd49b04a79487ec88c2cf81d98
Ludozzz1 <ludovico.maltagliati@gmail.com>
2023-04-20 20:13:53 +0200

    Added pickCards method to GameController

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -1,8 +1,5 @@
 package controller;
-import model.Cockade;
-import model.CommonObjective;
-import model.Game;
-import model.Player;
+import model.*;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -11,25 +8,36 @@ import java.util.Optional;
 
 public class GameController {
 
-    Game game;
-    boolean endGame;
+    private Game game;
+    private boolean endGame;
 
     /** Constructor that creates a new game with the specified players.
     * @author Ludovico
     * @param playersNames The names of the players
     */
-    public GameController(ArrayList<String> playersNames) {
+    private GameController(ArrayList<String> playersNames) {
         game = new Game(playersNames);
     }
 
-    public boolean checkFullShelf(Player player){
+    private boolean checkFullShelf(Player player){
         return player.getShelf().isFull();
     }
 
-    public boolean checkRefillTable(){
+    private void setEndGame(Player player){
+        if(checkFullShelf(player)){
+            endGame = true;
+        }
+    }
+
+    private boolean checkRefillTable(){
         return game.getTabletop().needRefill();
     }
 
+    private void refillTable(){
+        if (checkRefillTable()) {
+            game.getTabletop().fillTable();
+        }
+    }
 
     private void addPersonalCockade(Player player){
         Optional<Cockade> helpCockadePersonal = player.getPersonalObjective().isCompleted(player.getShelf());
@@ -62,6 +70,30 @@ public class GameController {
         Collections.sort(players, (player1, player2) -> player2.getPoints() - player1.getPoints());
         return players;
     }
+
+    private boolean pickCards(ArrayList<Integer> y, ArrayList<Integer> x, Player player, int column){
+        Card[] cards = new Card[y.size()];
+
+        if(y.size() != x.size() || y.size() > 3 || y.size() < 1 || x.size() > 3 || x.size() < 1) {
+            return false;
+        }
+
+        try{
+            for (int i = 0; i < y.size(); i++) {
+                game.getTabletop().isPickable(y.get(i), x.get(i));
+                cards[i] = game.getTabletop().pickCard(y.get(i), x.get(i));
+            }
+
+            player.getShelf().insert(column, cards);
+
+        }catch (Exception e) {
+            return false;
+        }
+
+        return true;
+    }
+
+
 
 }
 
