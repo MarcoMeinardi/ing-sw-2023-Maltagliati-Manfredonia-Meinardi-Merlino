8adf8e632abcf2d4e8d6b1fcf44f57d3eac81382
Ludozzz1 <ludovico.maltagliati@gmail.com>
2023-04-22 19:58:55 +0200

    refactored isComplete test

--- src/test/java/model/CommonObjectiveTest.java
+++ src/test/java/model/CommonObjectiveTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Optional;
 
 public class CommonObjectiveTest {
@@ -29,50 +30,21 @@ public class CommonObjectiveTest {
         final int NROWS = 6;
         final int NCOLS = 5;
         Shelf shelf = new Shelf();
-        ArrayList<Card> cards = new ArrayList<Card>();
         CommonObjective objective = new CommonObjective("6 groups of 2 cards", nPlayers, CommonObjective::sixGroupsOfTwoCards);
 
         Optional<Cockade> cockade = objective.isCompleted(shelf);
         assertEquals(false, cockade.isPresent());
 
-        for(int i = 0; i < 2; i++){
-            cards.add(Card.Pianta);
-        }
-        shelf.insert(0, cards);
-        cards.clear();
-
-        for(int i = 0; i < 2; i++){
-            cards.add(Card.Gatto);
-        }
-        shelf.insert(1, cards);
-        cards.clear();
-
-        for(int i = 0; i < 2; i++){
-            cards.add(Card.Gioco);
-        }
-        shelf.insert(2, cards);
-        cards.clear();
-
-        for(int i = 0; i < 2; i++){
-            cards.add(Card.Libro);
-        }
-        shelf.insert(3, cards);
-        cards.clear();
-
-        for(int i = 0; i < 2; i++){
-            cards.add(Card.Trofeo);
-        }
-        shelf.insert(4, cards);
-        cards.clear();
-
-        for (int i = 0; i < 2; i++) {
-            cards.add(Card.Cornice);
-        }
-        shelf.insert(4, cards);
-        cards.clear();
+        shelf.insert(0,  new ArrayList<Card>(Arrays.asList(Card.Pianta, Card.Pianta)));
+        shelf.insert(1,  new ArrayList<Card>(Arrays.asList(Card.Gatto, Card.Gatto)));
+        shelf.insert(2,  new ArrayList<Card>(Arrays.asList(Card.Gioco, Card.Gioco)));
+        shelf.insert(3,  new ArrayList<Card>(Arrays.asList(Card.Libro, Card.Libro)));
+        shelf.insert(4,  new ArrayList<Card>(Arrays.asList(Card.Trofeo, Card.Trofeo)));
+        shelf.insert(4,  new ArrayList<Card>(Arrays.asList(Card.Cornice, Card.Cornice)));
 
         cockade = objective.isCompleted(shelf);
         assertEquals(true, cockade.isPresent());
+
 
 
     }
