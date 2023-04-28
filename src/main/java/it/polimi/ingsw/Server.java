2b1fb6e89d37779aa141c4a729057e43a620247e
MarcoMeinardi <marco.meinardi@mail.polimi.it>
2023-04-28 12:34:27 +0200

    Swapped private and static keywords for consinstency

--- src/main/java/it/polimi/ingsw/Server.java
+++ src/main/java/it/polimi/ingsw/Server.java
@@ -4,8 +4,8 @@ import controller.lobby.LobbyController;
 import network.rpc.server.ClientManager;
 
 public class Server {
-	static private ClientManager clientManager = ClientManager.getInstance();
-	static private LobbyController lobbyController = LobbyController.getInstance();
+	private static ClientManager clientManager = ClientManager.getInstance();
+	private static LobbyController lobbyController = LobbyController.getInstance();
 
 	static public void main(String[] args) throws InterruptedException {
 		clientManager.join();
