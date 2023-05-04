83524d69bef0e0643a80d8a8d988423ea0cc7cc8
MarcoMeinardi <marco.meinardi@mail.polimi.it>
2023-05-04 20:07:13 +0200

    Implemented non blocking IO

--- src/main/java/cli/Utils.java
+++ src/main/java/cli/Utils.java
@@ -1,13 +1,12 @@
 package cli;
 
+import java.util.Optional;
+
+import network.rpc.client.NetworkManager;
 
 public class Utils {
 	private static NonBlockingIO IO = NonBlockingIO.getInstance();
 
-	public static String askString(String message) {
-		System.out.print(message);
-		return askString();
-	}
 	public static String askString() {
 		IO.ask();
 		try {
@@ -19,11 +18,29 @@ public class Utils {
 		}
 		return IO.getResult();
 	}
-
-	public static int askInt(String message) {
+	public static String askString(String message) {
 		System.out.print(message);
-		return askInt();
+		return askString();
 	}
+	public static Optional<String> askStringOrEvent() {
+		IO.ask();
+		try {
+			while (!IO.isAvailable()) {
+				if (NetworkManager.getInstance().hasEvent()) {
+					return Optional.empty();
+				}
+				Thread.sleep(50);
+			}
+		} catch (InterruptedException e) {
+			return Optional.empty();
+		}
+		return Optional.of(IO.getResult());
+	}
+	public static Optional<String> askStringOrEvent(String message) {
+		System.out.print(message);
+		return askStringOrEvent();
+	}
+
 	public static int askInt() {
 		int result = -1;
 		String line = askString();
@@ -31,6 +48,25 @@ public class Utils {
 			result = Integer.parseInt(line);
 		} catch (Exception e) {}
 		return result;
+	}
+	public static int askInt(String message) {
+		System.out.print(message);
+		return askInt();
+	}
+	public static Optional<Integer> askIntOrEvent() {
+		Optional<String> line = askStringOrEvent();
+		if (line.isPresent()) {
+			try {
+				return Optional.of(Integer.parseInt(line.get()));
+			} catch (Exception e) {
+				return Optional.of(-1);
+			}
+		}
+		return Optional.empty();
+	}
+	public static Optional<Integer> askIntOrEvent(String message) {
+		System.out.print(message);
+		return askIntOrEvent();
 	}
 
 	private static <E extends Enum<E>> String enumToOption(E e) {
@@ -47,6 +83,24 @@ public class Utils {
 			int selected = askInt() - 1;
 			if (selected >= 0 && selected < options.length) {
 				return options[selected];
+			} else {
+				System.out.println("[!] Invalid option");
+			}
+		}
+	}
+	public static <E extends Enum<E>> Optional<E> askOptionOrEvent(Class<E> enumClass) {
+		while (true) {
+			E[] options = enumClass.getEnumConstants();
+			for (int i = 0; i < options.length; i++) {
+				System.out.println(String.format("[%d] %s", i + 1, enumToOption(options[i])));
+			}
+			Optional<Integer> option = askIntOrEvent();
+			if (option.isEmpty()) {
+				return Optional.empty();
+			}
+			int selected = option.get() - 1;
+			if (selected >= 0 && selected < options.length) {
+				return Optional.of(options[selected]);
 			} else {
 				System.out.println("[!] Invalid option");
 			}
