c560ae5661119691521a81eb5fe1a55175af2b60
MarcoMeinardi <marco.meinardi@mail.polimi.it>
2023-05-06 23:03:06 +0200

    Fixed Lorenzo's silly indentation

--- src/main/java/network/rpc/server/Client.java
+++ src/main/java/network/rpc/server/Client.java
@@ -15,169 +15,169 @@ import java.util.function.BiFunction;
 import java.util.logging.Logger;
 
 public class Client extends Thread implements ClientInterface {
-        private final Socket socket;
-        private final ObjectInputStream incomingMessages;
-        private final ObjectOutputStream outcomingMessages;
-        private ClientStatusHandler statusHandler;
-        private BiFunction<Call<Serializable>, ClientInterface, Result<Serializable>> handler;
-        private Object handlerLock = new Object();
-        private LocalDateTime lastMessageTime = LocalDateTime.now();
-        private Object lastMessageTimeLock = new Object();
-        private String username = null;
-        protected static final int TIMEOUT = 60;
+	private final Socket socket;
+	private final ObjectInputStream incomingMessages;
+	private final ObjectOutputStream outcomingMessages;
+	private ClientStatusHandler statusHandler;
+	private BiFunction<Call<Serializable>, ClientInterface, Result<Serializable>> handler;
+	private Object handlerLock = new Object();
+	private LocalDateTime lastMessageTime = LocalDateTime.now();
+	private Object lastMessageTimeLock = new Object();
+	private String username = null;
+	protected static final int TIMEOUT = 60;
 
-        public Client(Socket socket, BiFunction<Call<Serializable>,ClientInterface,Result<Serializable>> handler) throws Exception {
-            this.socket = socket;
-            this.incomingMessages = new ObjectInputStream(socket.getInputStream());
-            this.outcomingMessages = new ObjectOutputStream(socket.getOutputStream());
-            this.handler = handler;
-            this.statusHandler = new ClientStatusHandler();
-        }
+	public Client(Socket socket, BiFunction<Call<Serializable>,ClientInterface,Result<Serializable>> handler) throws Exception {
+		this.socket = socket;
+		this.incomingMessages = new ObjectInputStream(socket.getInputStream());
+		this.outcomingMessages = new ObjectOutputStream(socket.getOutputStream());
+		this.handler = handler;
+		this.statusHandler = new ClientStatusHandler();
+	}
 
-        public ClientStatus getStatus() {
-            return statusHandler.getStatus();
-        }
+	public ClientStatus getStatus() {
+		return statusHandler.getStatus();
+	}
 
-        public void setStatus(ClientStatus status) {
-            statusHandler.setStatus(status);
-        }
+	public void setStatus(ClientStatus status) {
+		statusHandler.setStatus(status);
+	}
 
-        public ClientStatus getLastValidStatus() {
-            return statusHandler.getLastValidStatus();
-        }
+	public ClientStatus getLastValidStatus() {
+		return statusHandler.getLastValidStatus();
+	}
 
-        public void setLastValidStatus(ClientStatus status) {
-            statusHandler.setLastValidStatus(status);
-        }
+	public void setLastValidStatus(ClientStatus status) {
+		statusHandler.setLastValidStatus(status);
+	}
 
-        public <T extends Serializable> void send(ServerEvent<T> message) throws DisconnectedClientException {
-            if(getStatus() == ClientStatus.Disconnected){
-                throw new DisconnectedClientException();
-            }
-            synchronized (this.outcomingMessages){
-                try{
-                    outcomingMessages.reset();
-                    Result<Serializable> result = Result.serverPush(message);
-                    outcomingMessages.writeObject((Object)result);
-                }catch(Exception e){
-                    Logger.getLogger(Client.class.getName()).warning(e.getMessage());
-                    disconnect();
-                    throw new DisconnectedClientException();
-                }
-            }
-        }
+	public <T extends Serializable> void send(ServerEvent<T> message) throws DisconnectedClientException {
+		if(getStatus() == ClientStatus.Disconnected){
+			throw new DisconnectedClientException();
+		}
+		synchronized (this.outcomingMessages){
+			try{
+				outcomingMessages.reset();
+				Result<Serializable> result = Result.serverPush(message);
+				outcomingMessages.writeObject((Object)result);
+			}catch(Exception e){
+				Logger.getLogger(Client.class.getName()).warning(e.getMessage());
+				disconnect();
+				throw new DisconnectedClientException();
+			}
+		}
+	}
 
-    private <T extends Serializable> void send(Result<T> message) throws DisconnectedClientException {
-        if(getStatus() == ClientStatus.Disconnected){
-            throw new DisconnectedClientException();
-        }
-        synchronized (this.outcomingMessages){
-            try{
-                outcomingMessages.reset();
-                outcomingMessages.writeObject((Object)message);
-            }catch(Exception e){
-                Logger.getLogger(Client.class.getName()).warning(e.getMessage());
-                disconnect();
-                throw new DisconnectedClientException();
-            }
-        }
-    }
+	private <T extends Serializable> void send(Result<T> message) throws DisconnectedClientException {
+		if(getStatus() == ClientStatus.Disconnected){
+			throw new DisconnectedClientException();
+		}
+		synchronized (this.outcomingMessages){
+			try{
+				outcomingMessages.reset();
+				outcomingMessages.writeObject((Object)message);
+			}catch(Exception e){
+				Logger.getLogger(Client.class.getName()).warning(e.getMessage());
+				disconnect();
+				throw new DisconnectedClientException();
+			}
+		}
+	}
 
-        private <T extends Serializable> Call<T> receive() throws DisconnectedClientException{
-            if(getStatus() == ClientStatus.Disconnected){
-                throw new DisconnectedClientException();
-            }
-            synchronized (this.incomingMessages){
-                try{
-                    Object obj = this.incomingMessages.readObject();
-                    if(obj instanceof Call){
-                        return (Call)obj;
-                    }
-                }catch(Exception e){
-                    Logger.getLogger(Client.class.getName()).warning(e.getMessage());
-                }
-                disconnect();
-                throw new DisconnectedClientException();
-            }
-        }
+	private <T extends Serializable> Call<T> receive() throws DisconnectedClientException{
+		if(getStatus() == ClientStatus.Disconnected){
+			throw new DisconnectedClientException();
+		}
+		synchronized (this.incomingMessages){
+			try{
+				Object obj = this.incomingMessages.readObject();
+				if(obj instanceof Call){
+					return (Call)obj;
+				}
+			}catch(Exception e){
+				Logger.getLogger(Client.class.getName()).warning(e.getMessage());
+			}
+			disconnect();
+			throw new DisconnectedClientException();
+		}
+	}
 
-        public void disconnect(){
-            setStatus(ClientStatus.Disconnected);
-            synchronized (socket){
-                try{
-                    socket.close();
-                }catch (IOException e){
-                    Logger.getLogger(Client.class.getName()).warning(e.getMessage());
-                }
-            }
-        }
+	public void disconnect(){
+		setStatus(ClientStatus.Disconnected);
+		synchronized (socket){
+			try{
+				socket.close();
+			}catch (IOException e){
+				Logger.getLogger(Client.class.getName()).warning(e.getMessage());
+			}
+		}
+	}
 
-        public boolean isDisconnected(){
-            return getStatus() == ClientStatus.Disconnected;
-        }
-        
-        public boolean checkPing() {
-            if(getStatus() == ClientStatus.Disconnected){
-                return false;
-            }
-            synchronized (lastMessageTimeLock) {
-                if(lastMessageTime.plusSeconds(TIMEOUT).isBefore(LocalDateTime.now())){
-                    disconnect();
-                    return false;
-                }
-            }
-            return true;
-        }
+	public boolean isDisconnected(){
+		return getStatus() == ClientStatus.Disconnected;
+	}
+	
+	public boolean checkPing() {
+		if(getStatus() == ClientStatus.Disconnected){
+			return false;
+		}
+		synchronized (lastMessageTimeLock) {
+			if(lastMessageTime.plusSeconds(TIMEOUT).isBefore(LocalDateTime.now())){
+				disconnect();
+				return false;
+			}
+		}
+		return true;
+	}
 
-        @Override
-        public void run() {
-            while (getStatus() != ClientStatus.Disconnected) {
-                try {
-                    Call call = receive();
-                    synchronized (lastMessageTimeLock){
-                        lastMessageTime = LocalDateTime.now();
-                    }
-                    if(call.service() == Service.Ping){
-                        send(Result.ok(true, call.id()));
-                    }else{
-                        Result result = handler.apply(call, this);
-                        send(result);
-                    }
-                } catch (DisconnectedClientException e) {
-                    Logger.getLogger(Client.class.getName()).warning(e.getMessage());
-                }
-            }
-        }
+	@Override
+	public void run() {
+		while (getStatus() != ClientStatus.Disconnected) {
+			try {
+				Call call = receive();
+				synchronized (lastMessageTimeLock){
+					lastMessageTime = LocalDateTime.now();
+				}
+				if(call.service() == Service.Ping){
+					send(Result.ok(true, call.id()));
+				}else{
+					Result result = handler.apply(call, this);
+					send(result);
+				}
+			} catch (DisconnectedClientException e) {
+				Logger.getLogger(Client.class.getName()).warning(e.getMessage());
+			}
+		}
+	}
 
-        public void setCallHandler(BiFunction<Call<Serializable>, ClientInterface, Result<Serializable>> handler){
-            synchronized (this.handlerLock) {
-                this.handler = handler;
-            }
-        }
+	public void setCallHandler(BiFunction<Call<Serializable>, ClientInterface, Result<Serializable>> handler){
+		synchronized (this.handlerLock) {
+			this.handler = handler;
+		}
+	}
 
-        protected void setUsername(String username) throws ClientAlreadyConnectedExeption {
-            if(this.username != null){
-                throw new ClientAlreadyConnectedExeption();
-            }
-            this.username = username;
-        }
+	protected void setUsername(String username) throws ClientAlreadyConnectedExeption {
+		if(this.username != null){
+			throw new ClientAlreadyConnectedExeption();
+		}
+		this.username = username;
+	}
 
-        public String getUsername() throws ClientNotIdentifiedException {
-            if(this.username == null){
-                throw new ClientNotIdentifiedException();
-            }
-            return this.username;
-        }
+	public String getUsername() throws ClientNotIdentifiedException {
+		if(this.username == null){
+			throw new ClientNotIdentifiedException();
+		}
+		return this.username;
+	}
 
-        public LocalDateTime getLastMessageTime(){
-            synchronized (lastMessageTimeLock){
-                return lastMessageTime;
-            }
-        }
+	public LocalDateTime getLastMessageTime(){
+		synchronized (lastMessageTimeLock){
+			return lastMessageTime;
+		}
+	}
 
-    public BiFunction<Call<Serializable>, ClientInterface, Result<Serializable>> getCallHandler() {
-        synchronized (this.handlerLock) {
-            return handler;
-        }
-    }
+	public BiFunction<Call<Serializable>, ClientInterface, Result<Serializable>> getCallHandler() {
+		synchronized (this.handlerLock) {
+			return handler;
+		}
+	}
 }
