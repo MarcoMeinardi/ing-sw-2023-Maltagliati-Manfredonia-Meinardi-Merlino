3c1f0e9c3c6dec36bdbd6dc99cd2009fb1006510
MarcoMeinardi <marco.meinardi@mail.polimi.it>
2023-05-08 13:23:20 +0200

    Error handling

--- src/main/java/it/polimi/ingsw/Server.java
+++ src/main/java/it/polimi/ingsw/Server.java
@@ -5,10 +5,10 @@ import network.ClientManagerInterface;
 import network.rpc.server.ClientManager;
 
 public class Server {
-	private static ClientManagerInterface clientManager = ClientManager.getInstance();
-	private static LobbyController lobbyController = LobbyController.getInstance();
 
-	static public void main(String[] args) throws InterruptedException {
+	static public void main(String[] args) throws Exception {
+		ClientManagerInterface clientManager = ClientManager.getInstance();
+		LobbyController lobbyController = LobbyController.getInstance();
 		clientManager.waitAndClose();
 	}
 }
