64506a391c2885a57cd8e9e846277be6c3ba80ee
Ludozzz1 <ludovico.maltagliati@gmail.com>
2023-05-26 16:01:55 +0200

    added utils for chat

--- src/main/java/view/gui/LobbyViewController.java
+++ src/main/java/view/gui/LobbyViewController.java
@@ -243,6 +243,7 @@ public class LobbyViewController implements Initializable{
         }
     }
 
+
     /**
      * method called to send a message to the server and add it to the chat.
      * It checks if the message is valid and if it is not it returns after adding the
@@ -311,7 +312,7 @@ public class LobbyViewController implements Initializable{
                 }
                 Message message = new Message(username, messageText, players.getSelectionModel().getSelectedItem().toString());
                 players.getSelectionModel().clearSelection();
-                addMessageToChat(message);
+                utils.addMessageToChat(message, username, chat);
                 return;
             }
             Result result = networkManager.chat(new Message(username, messageText)).waitResult();
@@ -324,52 +325,11 @@ public class LobbyViewController implements Initializable{
                 return;
             }
             Message message = new Message(username, messageText);
-            addMessageToChat(message);
+            utils.addMessageToChat(message, username, chat);
         } catch (Exception e) {
             System.out.println("[ERROR] " + e.getMessage());
         }
 
-    }
-
-    /**
-     * method called to add a message to the chat.
-     * It automatically adds to the message the username of the sender
-     * and the hour and minute the message was sent.
-     * Scrolls the chat to the bottom to show the last message.
-     * It is called when the server sends a message to the lobby chat.
-     *
-     * @param message the message to add to the chat
-     * @autor: Ludovico
-     */
-
-    public void addMessageToChat(Message message){
-        Calendar calendar = GregorianCalendar.getInstance();
-        String hour = String.valueOf(calendar.get(Calendar.HOUR_OF_DAY));
-        String minute = String.valueOf(calendar.get(Calendar.MINUTE));
-        if(hour.length() == 1){
-            hour = "0" + hour;
-        }
-        if(minute.length() == 1){
-            minute = "0" + minute;
-        }
-        if (message.idReceiver().isEmpty()) {
-            chat.getItems().add(String.format("[%s:%s] %s to everyone: %s", hour, minute, message.idSender(), message.message()));
-        } else {
-            if(!message.idSender().equals(username)) {
-                chat.getItems().add(String.format("[%s:%s] %s to you: %s", hour, minute, message.idSender(), message.message()));
-            }
-            else{
-                if(message.idReceiver().get().equals(message.idSender())){
-                    chat.getItems().add(String.format("[%s:%s] %s to himself: you are a schizo", hour, minute, message.idSender()));
-                }
-                else {
-                    chat.getItems().add(String.format("[%s:%s] you to %s: %s", hour, minute, message.idReceiver().get(), message.message()));
-                }
-            }
-        }
-        if(chat.getItems().size() != 3){
-            chat.scrollTo(chat.getItems().size()-1);
-        }
     }
 
     /**
@@ -431,7 +391,7 @@ public class LobbyViewController implements Initializable{
                     Platform.runLater(new Runnable() {
                         @Override
                         public void run() {
-                            addMessageToChat(message);
+                            utils.addMessageToChat(message, username, chat);
                         }
                     });
                 }
