622c4fdfb178d0f895180ffc9dc833aa02ac1bf1
Doso <80822398+manfroso@users.noreply.github.com>
2023-05-27 16:01:50 +0200

    probably a COC ty @ludov

--- src/main/java/view/gui/CommonObjController.java
+++ src/main/java/view/gui/CommonObjController.java
@@ -3,6 +3,9 @@ package view.gui;
 import controller.lobby.Lobby;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
 import model.Cockade;
 import model.CommonObjective;
 import model.Game;
@@ -10,6 +13,7 @@ import model.Shelf;
 import network.ClientStatus;
 import network.NetworkManagerInterface;
 
+import java.awt.*;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
@@ -17,11 +21,122 @@ import java.util.ResourceBundle;
 public class CommonObjController implements Initializable {
     private ArrayList<String> commonObjectives;
     private GameData gameData;
+    @FXML
+    private VBox imageContainer;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-         GameData gameData = GameViewController.getGameData();
-         commonObjectives = gameData.getCommonObjectives();
-         //TODO scorri le common objectives e crea un'immagine per ognuna. Usa i nomi delle common objectives per caricare le immagini, ES: "all equal corners"--> 8.jpg. Usa chatgpt per capire come mettere una immagine nel fxml
+        GameData gameData = GameViewController.getGameData();
+        commonObjectives = gameData.getCommonObjectives();
+        for (int i = 0; i < commonObjectives.size(); i++) {
+            if (commonObjectives.get(i).equals("2 square-shaped groups")) {
+                String imageName = 1 + ".jpg";
+                String imagePath = getClass().getResource(imageName).toExternalForm();
+                Image image = new Image(imagePath);
+                ImageView imageView = new ImageView(image);
+                imageView.setFitWidth(100);
+                imageView.setFitHeight(100);
+                imageContainer.getChildren().add(imageView);
+            }
+            if (commonObjectives.get(i).equals("2 columns of 6 different cards")) {
+                String imageName = 2 + ".jpg";
+                String imagePath = getClass().getResource(imageName).toExternalForm();
+                Image image = new Image(imagePath);
+                ImageView imageView = new ImageView(image);
+                imageView.setFitWidth(100);
+                imageView.setFitHeight(100);
+                imageContainer.getChildren().add(imageView);
+            }
+            if (commonObjectives.get(i).equals("4 groups of 4 cards")) {
+                String imageName = 3 + ".jpg";
+                String imagePath = getClass().getResource(imageName).toExternalForm();
+                Image image = new Image(imagePath);
+                ImageView imageView = new ImageView(image);
+                imageView.setFitWidth(100);
+                imageView.setFitHeight(100);
+                imageContainer.getChildren().add(imageView);
+            }
+            if (commonObjectives.get(i).equals("6 groups of 2 cards")) {
+                String imageName = 4 + ".jpg";
+                String imagePath = getClass().getResource(imageName).toExternalForm();
+                Image image = new Image(imagePath);
+                ImageView imageView = new ImageView(image);
+                imageView.setFitWidth(100);
+                imageView.setFitHeight(100);
+                imageContainer.getChildren().add(imageView);
+            }
+            if (commonObjectives.get(i).equals("3 columns of at most 3 different cards")) {
+                String imageName = 5 + ".jpg";
+                String imagePath = getClass().getResource(imageName).toExternalForm();
+                Image image = new Image(imagePath);
+                ImageView imageView = new ImageView(image);
+                imageView.setFitWidth(100);
+                imageView.setFitHeight(100);
+                imageContainer.getChildren().add(imageView);
+            }
+            if (commonObjectives.get(i).equals("2 rows with 5 different cards")) {
+                String imageName = 6 + ".jpg";
+                String imagePath = getClass().getResource(imageName).toExternalForm();
+                Image image = new Image(imagePath);
+                ImageView imageView = new ImageView(image);
+                imageView.setFitWidth(100);
+                imageView.setFitHeight(100);
+                imageContainer.getChildren().add(imageView);
+            }
+            if (commonObjectives.get(i).equals("4 row of at most 3 different cards")) {
+                String imageName = 7 + ".jpg";
+                String imagePath = getClass().getResource(imageName).toExternalForm();
+                Image image = new Image(imagePath);
+                ImageView imageView = new ImageView(image);
+                imageView.setFitWidth(100);
+                imageView.setFitHeight(100);
+                imageContainer.getChildren().add(imageView);
+            }
+            if (commonObjectives.get(i).equals("all equal corner")) {
+                String imageName = 8 + ".jpg";
+                String imagePath = getClass().getResource(imageName).toExternalForm();
+                Image image = new Image(imagePath);
+                ImageView imageView = new ImageView(image);
+                imageView.setFitWidth(100);
+                imageView.setFitHeight(100);
+                imageContainer.getChildren().add(imageView);
+            }
+            if (commonObjectives.get(i).equals("eight equal cards")) {
+                String imageName = 9 + ".jpg";
+                String imagePath = getClass().getResource(imageName).toExternalForm();
+                Image image = new Image(imagePath);
+                ImageView imageView = new ImageView(image);
+                imageView.setFitWidth(100);
+                imageView.setFitHeight(100);
+                imageContainer.getChildren().add(imageView);
+            }
+            if (commonObjectives.get(i).equals("X shapes group")) {
+                String imageName = 10 + ".jpg";
+                String imagePath = getClass().getResource(imageName).toExternalForm();
+                Image image = new Image(imagePath);
+                ImageView imageView = new ImageView(image);
+                imageView.setFitWidth(100);
+                imageView.setFitHeight(100);
+                imageContainer.getChildren().add(imageView);
+            }
+            if (commonObjectives.get(i).equals("5 cards in diagonal")) {
+                String imageName = 11 + ".jpg";
+                String imagePath = getClass().getResource(imageName).toExternalForm();
+                Image image = new Image(imagePath);
+                ImageView imageView = new ImageView(image);
+                imageView.setFitWidth(100);
+                imageView.setFitHeight(100);
+                imageContainer.getChildren().add(imageView);
+            }
+            if (commonObjectives.get(i).equals("stair-shaped cards")) {
+                String imageName = 12 + ".jpg";
+                String imagePath = getClass().getResource(imageName).toExternalForm();
+                Image image = new Image(imagePath);
+                ImageView imageView = new ImageView(image);
+                imageView.setFitWidth(100);
+                imageView.setFitHeight(100);
+                imageContainer.getChildren().add(imageView);
+            }
+        }
     }
 
 }
