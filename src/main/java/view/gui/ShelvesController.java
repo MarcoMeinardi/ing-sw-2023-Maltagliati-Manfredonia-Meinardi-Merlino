5f02969156b595babb09065993b735ae1d426136
Doso <80822398+manfroso@users.noreply.github.com>
2023-05-28 11:01:40 +0200

    rip button ShelvesController

--- src/main/java/view/gui/ShelvesController.java
+++ src/main/java/view/gui/ShelvesController.java
@@ -1,8 +1,11 @@
 package view.gui;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import model.Shelf;
 
 import java.net.URL;
@@ -15,23 +18,27 @@ public class ShelvesController implements Initializable {
     private ArrayList<String> playersNames;
 
     private static String me;
-    private static Shelf myShelf;
+    private static Shelf Shelf;
+
+    @FXML
+    private HBox imageContainer;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         GameData gameData = GameViewController.getGameData();
         shelves = gameData.getShelves();
         playersNames = gameData.getPlayersNames();
-        for (int i = 0; i < shelves.size(); i++) {
-            if (playersNames.get(i).equals(me)) {
-                myShelf = shelves.get(i);
-                String imageName = "/img/board/shelf.jpg";
-                String imagePath = getClass().getResource(imageName).toExternalForm();
-                Image image = new Image(imagePath);
-                ImageView imageView = new ImageView(image);
-                imageView.setFitWidth(200);
-                imageView.setFitHeight(200);
-            }
+        for (int i = 0; i < playersNames.size(); i++) {
+            String imageName = "/img/board/bookshelf.jpg";
+            Label label = new Label(playersNames.get(i));
+            label.setStyle("-fx-font-size: 20px; -fx-font-weight: bold");
+            String imagePath = getClass().getResource(imageName).toExternalForm();
+            Image image = new Image(imagePath);
+            ImageView imageView = new ImageView(image);
+            imageView.setFitWidth(200);
+            imageView.setFitHeight(200);
+            imageContainer.getChildren().add(imageView);
+            imageContainer.getChildren().add(label);
         }
     }
 }
