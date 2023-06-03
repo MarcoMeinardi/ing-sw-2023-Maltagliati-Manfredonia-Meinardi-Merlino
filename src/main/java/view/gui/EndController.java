ba9a08302ff3da90555dd8017a1db15a7fd3d3b8
Ludozzz1 <ludovico.maltagliati@gmail.com>
2023-06-03 20:05:12 +0200

    many small changes + found many fun bugs

--- src/main/java/view/gui/EndController.java
+++ src/main/java/view/gui/EndController.java
@@ -1,15 +1,32 @@
 package view.gui;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import model.Card;
 import model.Score;
 import model.ScoreBoard;
+import model.Shelf;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class EndController implements Initializable {
+    private static final int WIDTH = 1140;
+    private static final int HEIGHT = 760;
     private static ScoreBoard scoreBoard;
     private static String username;
     @FXML
@@ -22,6 +39,10 @@ public class EndController implements Initializable {
     private Label messageLabel4;
     @FXML
     private Label titleLabel;
+    @FXML
+    private Button exitButton;
+    private Stage stage;
+    private Scene scene;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         this.scoreBoard = GameViewController.gameData.getScoreBoard();
@@ -53,4 +74,16 @@ public class EndController implements Initializable {
             position++;
         }
     }
+
+    @FXML
+    private void goToLobbies(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/MainMenu.fxml"));
+        stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+        scene = new Scene(root, WIDTH, HEIGHT);
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
 }
