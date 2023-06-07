945a6ca85e37c4ad98f2587014969dc0e9f48c77
MarcoMeinardi <marco.meinardi@mail.polimi.it>
2023-06-07 10:51:10 +0200

    Renamed variables in scorebord according to Java (ew) convention

--- src/main/java/model/ScoreBoard.java
+++ src/main/java/model/ScoreBoard.java
@@ -19,13 +19,13 @@ public class ScoreBoard implements Serializable, Iterable<Score> {
 
     private final ArrayList<Score> scores;
 	private final String winner;
-    private final String most_cats;
-    private final String most_books;
-    private final String most_games;
-    private final String most_frames;
-    private final String most_trophies;
-    private final String most_plants;
-    private final String sole_survivor;
+    private final String mostCats;
+    private final String mostBooks;
+    private final String mostGames;
+    private final String mostFrames;
+    private final String mostTrophies;
+    private final String mostPlants;
+    private final String soleSurvivor;
 
     private final VictoryType victoryType;
 
@@ -33,13 +33,13 @@ public class ScoreBoard implements Serializable, Iterable<Score> {
         scores = new ArrayList<>();
         ArrayList<Player> players = game.finalRanks();
         winner = players.get(0).getName();
-        most_cats = players.stream().max(Comparator.comparingInt(p -> p.getShelf().countCard(Card.Type.Gatto))).get().getName();
-        most_books = players.stream().max(Comparator.comparingInt(p -> p.getShelf().countCard(Card.Type.Libro))).get().getName();
-        most_games = players.stream().max(Comparator.comparingInt(p -> p.getShelf().countCard(Card.Type.Gioco))).get().getName();
-        most_frames = players.stream().max(Comparator.comparingInt(p -> p.getShelf().countCard(Card.Type.Cornice))).get().getName();
-        most_trophies = players.stream().max(Comparator.comparingInt(p -> p.getShelf().countCard(Card.Type.Trofeo))).get().getName();
-        most_plants = players.stream().max(Comparator.comparingInt(p -> p.getShelf().countCard(Card.Type.Pianta))).get().getName();
-        sole_survivor = findSoleSurvivor(players);
+        mostCats = players.stream().max(Comparator.comparingInt(p -> p.getShelf().countCard(Card.Type.Gatto))).get().getName();
+        mostBooks = players.stream().max(Comparator.comparingInt(p -> p.getShelf().countCard(Card.Type.Libro))).get().getName();
+        mostGames = players.stream().max(Comparator.comparingInt(p -> p.getShelf().countCard(Card.Type.Gioco))).get().getName();
+        mostFrames = players.stream().max(Comparator.comparingInt(p -> p.getShelf().countCard(Card.Type.Cornice))).get().getName();
+        mostTrophies = players.stream().max(Comparator.comparingInt(p -> p.getShelf().countCard(Card.Type.Trofeo))).get().getName();
+        mostPlants = players.stream().max(Comparator.comparingInt(p -> p.getShelf().countCard(Card.Type.Pianta))).get().getName();
+        soleSurvivor = findSoleSurvivor(players);
         victoryType = findVictoryType(players);
         for (Player player : players) {
             Score score = createScore(player);
@@ -85,8 +85,8 @@ public class ScoreBoard implements Serializable, Iterable<Score> {
     private Score createScore(Player player) {
         String playerName = player.getName();
         String title = "";
-        if (sole_survivor != null) {
-            if (playerName.equals(sole_survivor)) {
+        if (soleSurvivor != null) {
+            if (playerName.equals(soleSurvivor)) {
                 title = "Many fall in the face of chaos; but not this one, not today.";
             } else {
                 title = "Madness, our old friend!";
@@ -104,17 +104,17 @@ public class ScoreBoard implements Serializable, Iterable<Score> {
                         title = "The risk was calculated but, boy am I bad at math.";
                     }
                 }
-            } else if (playerName.equals(most_cats)) {
+            } else if (playerName.equals(mostCats)) {
                 title = "Can Mew Imyagine An Imyaginyary Mewnyagerie Mewnyager Imyagining Mewnyaging An Imyaginyary Mewnyagerie.";
-            } else if (playerName.equals(most_trophies)) {
+            } else if (playerName.equals(mostTrophies)) {
                 title = "That's a lot of trophies for someone who didn't win.";
-            }else if (playerName.equals(most_books)) {
+            }else if (playerName.equals(mostBooks)) {
                 title = "Shelf full of books? Well not that you had any winner trophy to put on it anyway.";
-            } else if (playerName.equals(most_games)) {
+            } else if (playerName.equals(mostGames)) {
                 title = "Nice guys finish last. Am I right gamer?";
-            } else if (playerName.equals(most_frames)) {
+            } else if (playerName.equals(mostFrames)) {
                 title = "May your frames be many and your photos few.";
-            }  else if (playerName.equals(most_plants)) {
+            }  else if (playerName.equals(mostPlants)) {
                 title = "You took the plants? Good, your brain could use some extra oxygen.";
             } else {
                 title = "NO TITLE FOR YOU! Come back, one year!";
--- src/test/java/model/ScoreBoardBuilderTest.java
+++
@@ -1,3 +0,0 @@
-package model;
-
-public class ScoreBoardBuilderTest {}
