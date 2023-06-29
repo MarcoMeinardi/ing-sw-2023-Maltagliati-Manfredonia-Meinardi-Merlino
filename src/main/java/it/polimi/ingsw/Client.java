e488a88aa085b69507a9911bf3714d158b4265f7
MarcoMeinardi <marco.meinardi@mail.polimi.it>
2023-06-29 14:06:46 +0200

    Disable logging

--- src/main/java/it/polimi/ingsw/Client.java
+++ src/main/java/it/polimi/ingsw/Client.java
@@ -5,10 +5,13 @@ import view.gui.Main;
 
 import java.util.Arrays;
 import java.util.logging.Logger;
+import java.util.logging.Level;
 
 public class Client {
 
 	public static void main(String[] args) {
+		Logger root = Logger.getLogger("");
+		root.setLevel(Level.OFF);
 		if (Arrays.asList(args).contains("-cli")) {
 			CLI cli = CLI.getInstance();
 			cli.run();
--- src/main/java/it/polimi/ingsw/Server.java
+++ src/main/java/it/polimi/ingsw/Server.java
@@ -6,18 +6,23 @@ import network.GlobalClientManager;
 
 import java.util.Scanner;
 import java.util.logging.Logger;
+import java.util.logging.Level;
 import java.util.regex.Pattern;
 
 public class Server {
 	static private final Logger logger = Logger.getLogger(Server.class.getName());
 	static private final Pattern ip4Pattern = Pattern.compile("^((25[0-5]|(2[0-4]|1[0-9]|[1-9]|)[0-9])(\\.(?!$)|$)){4}$");
 	static private final Pattern ip6Pattern = Pattern.compile("^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$");
+
 	static public void main(String[] args) throws Exception {
+		Logger root = Logger.getLogger("");
+		root.setLevel(Level.OFF);
 		System.setProperty("java.rmi.server.hostname", getIp());
 		ClientManagerInterface clientManager = GlobalClientManager.getInstance();
 		LobbyController lobbyController = LobbyController.getInstance();
 		clientManager.waitAndClose();
 	}
+
 
 	static private String getIp(){
 		String ip;
